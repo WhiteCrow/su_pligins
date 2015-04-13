e27d81c50a5db2cf34b0c9eae4c0cf160c4ca95e
LiuSihao <liusihao123@qq.com>
2015-04-13 13:49:51 +0800

    fix switch window method and add redraw method.

--- component.rb
+++ component.rb
@@ -31,21 +31,32 @@ def init_window(definition)
   dialog
 end
 
+def selected_component_definition
+  selection = Sketchup.active_model.selection
+  @_selected_instance = selection.first
+  @_selected_component_definition =
+  if selection.count == 1 && @_selected_instance.class == Sketchup::ComponentInstance
+    selection.first.definition
+  else
+    nil
+  end
+end
+
 def switch_window
-  definition = selected_component_definition
-  return UI.messagebox("请选择一个动态组件") if definition.nil?
-  @_dlg = init_window(definition)
+  if @_selected_component_definition.nil? || @_selected_instance !=  Sketchup.active_model.selection.first
+    definition = selected_component_definition
+    @_dlg = init_window(definition)
+  end
+  return UI.messagebox("请选择一个动态组件") if @_selected_component_definition.nil?
   @_dlg.visible? ? @_dlg.close : @_dlg.show
 end
 
 def config_html_str(definition)
   return @_html_string if definition.nil?
-
-  attrs = selected_component_attrs(definition)
-
+  attrs = component_attrs(definition)
   return @_html_string if attrs.nil?
+  attrs = component_customized_attrs(attrs)
 
-  attrs = selected_component_customized_attrs(attrs)
   definition_id = definition.object_id.to_s
   @_html_string << "<a style=\"color: blue;\" onclick=\"expandDefinition(#{definition_id});\"><strong> > #{definition.name} </strong></a><br />"
   if @_selected_component_definition == definition
@@ -53,9 +64,9 @@ def config_html_str(definition)
   else
     @_html_string << "<div id=#{definition_id} style=\"display: none;\">"
   end
-  @_html_string << "名字<input value=#{definition.name}></input><br/>"
+  @_html_string << "<span style=\"width: 100px; display: inline-block;\">名字(name)</span><input value=#{definition.name}></input><br/>"
   attrs.each_pair do |key, value|
-    @_html_string << "#{key}<input value=#{value}></input><br/>"
+    @_html_string << "<span style=\"width: 100px; display: inline-block;\">#{key[0]}(#{key[1]})</span><input value=#{value}></input><br/>"
   end
 
   @_html_string << "<div style=\"margin-left: 10px\">"
@@ -69,29 +80,42 @@ def set_config_options(dialog, definition)
   config_html_str(definition)
   dialog.execute_script("$('#content #config-options').html('#{@_html_string}')")
 end
-def selected_component_definition
-  selection = Sketchup.active_model.selection
-  @_selected_component_definition =
-  if selection.count == 1 && selection.first.class == Sketchup::ComponentInstance
-    selection.first.definition
-  else
-    nil
+
+def redraw(definition)
+  definition.instances.each do |instance|
+    $dc_observers.get_latest_class.redraw(instance)
   end
+  definition
 end
 
-def selected_component_attrs(definition)
+def component_attrs(definition)
   dict = definition.attribute_dictionaries
   dict.nil? ? nil : dict.map{|d| d }.first
 end
 
-def selected_component_customized_attrs(attrs)
+def update_component_attrs(definition, new_attrs)
+  # update definition name first
+  if definition.name != new_attrs["name"]
+    definition.name = new_attrs["name"]
+  end
+  # update other attributes
+  old_attrs = component_attrs(definition)
+  new_attrs.each_pair do |key, value|
+    if old_attrs[key] != value
+      old_attrs[key] = value
+    end
+  end
+  definition
+end
+
+def component_customized_attrs(attrs)
   customized_attributes ||= {}
   attrs.each_pair do |key, value|
     if key=~ /_formlabel$/ && !value.nil?
       customized_key = value
       origin_key = key.gsub(/^_|_formlabel$/, '')
       customized_value = attrs[origin_key]
-      customized_attributes.merge!(customized_key => customized_value)
+      customized_attributes.merge!([customized_key, origin_key] => customized_value)
     end
   end
   customized_attributes
